f2ce2aad82903376bd5a2849d11c9eaf4d4c57e4
Hana Klingová <hana.klingova@gmail.com>
2018-07-11 22:29:58 +0200

    separete content into BookSearch component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 //import * as BooksAPI from './BooksAPI'
 import './App.css';
+import BookSearch from './BookSearch';
 
 class BooksApp extends React.Component {
   /**
@@ -14,7 +15,9 @@ class BooksApp extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      showSearchPage: false
+      currentlyReading: [],
+      wantToRead: [],
+      read: []
     };
   }
 
@@ -182,31 +185,12 @@ class BooksApp extends React.Component {
             </div>
           </div>
         )} />
-
+        
         <Route path='/search' render={() => (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link to='/' className="close-search" >Close</Link>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <BookSearch />
         )} />
 
-      </div>
+      </div> // end the parent div
     );
   }
 }
